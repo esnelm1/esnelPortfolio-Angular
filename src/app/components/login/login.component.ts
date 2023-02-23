f0238c40709c6404be7320dfc99f52b861aedfa1
esnel <esnelmassaro@hotmail.com>
2023-02-23 08:55:41 -0300

    agregado de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+    constructor(public dialog: MatDialog) {}
+  
+    openDialog() {
+      const dialogRef = this.dialog.open(LoginDialogComponent);
+  
+      dialogRef.afterClosed().subscribe(result => {
+        console.log(`Dialog result: ${result}`);
+      });
+    }
+  }
+    @Component({
+      selector: 'app-login-dialog',
+      templateUrl: './login-dialog.component.html',
+    })
+    export class LoginDialogComponent {
+  }
 
-}
