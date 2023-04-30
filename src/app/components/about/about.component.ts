1a35fffd8bbeab79a0f644449512e6ef96ec7d99
Esnel <esnelmassaro@hotmail.com>
2023-04-30 17:11:27 -0300

    ANDUVO EL LOGINNNN

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,23 +1,46 @@
 import { Component } from '@angular/core';
 import { PortfolioService } from 'src/app/services/portfolio.service';
+import { UserService } from 'src/app/services/user.service';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
-  aboutMe:any;
-  token:any;
-  constructor(private datosPortfolio:PortfolioService) {}
+export class AboutComponent implements OnInit {
+  aboutMe: any;
+  isLogged = false;
+  constructor(private datosPortfolio: PortfolioService, private userService: UserService) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.isLogged = true;
+        console.log(this.isLogged);
+        console.log(user.email);
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        // ...
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+
     this.datosPortfolio.obtenerDatos().subscribe(data => {
       this.aboutMe = data.aboutMe;
-    });    
-}
-onFileChanged(){
-  
-}
+    });
+
+  }
+  onFileChanged() {
+
+  }
+
 
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,10 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { UserService } from 'src/app/services/user.service';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 @Component({
   selector: 'app-login',
@@ -12,23 +14,23 @@ import { UserService } from 'src/app/services/user.service';
 
 export class LoginComponent {
 
-    constructor(public dialog: MatDialog) {}
-  
-    openDialog() {
-      const dialogRef = this.dialog.open(LoginDialogComponent);
-  
-      dialogRef.afterClosed().subscribe(result => {
-        console.log(`Dialog result: ${result}`);
-      });
-    }
+  constructor(public dialog: MatDialog) { }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(LoginDialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
-    @Component({
-      selector: 'app-login-dialog',
-      templateUrl: './login-dialog.component.html',
-    })
-    export class LoginDialogComponent implements OnInit {
-      disabled = true;
-      formLogin: FormGroup;
+}
+@Component({
+  selector: 'app-login-dialog',
+  templateUrl: './login-dialog.component.html',
+})
+export class LoginDialogComponent implements OnInit {
+  formLogin: FormGroup;
+  isLogged = false;
 
   constructor(
     private userService: UserService,
@@ -40,7 +42,27 @@ export class LoginComponent {
   }
 
   ngOnInit(): void {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.isLogged = true;
+        console.log(this.isLogged);
+        console.log(user.email);
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        // ...
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+
+
   }
+
 
   onSubmit() {
     this.userService.login(this.formLogin.value)
@@ -50,13 +72,14 @@ export class LoginComponent {
       .catch(error => console.log(error));
   }
 
-  
+
   onClick() {
     this.userService.logout()
-    .then(()=>{
-    })
+      .then(() => {
+      })
       .catch(error => console.log(error));
+      this.isLogged = false;
   }
-  
-  }
+
+}
 
